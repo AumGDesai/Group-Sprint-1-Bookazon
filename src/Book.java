870528bfb171b944aa4abde2b48a9f95f019004a
Jonathan <jongmears@gmail.com>
2024-09-30 19:39:22 -0400

    changed Item from interface to abstract class
    changed Item from interface to abstract class, made Book extend Item, and removed the now-redundant code from Book

--- src/Book.java
+++ src/Book.java
@@ -1,29 +1,16 @@
-public class Book {
-    private String title;
+public class Book extends Item {
     private String author;
     private int yearPublished;
-    private double price;
     private boolean isPaperback;  // true if the book is paperback, false if it is hardcover
 
     public Book(String title, String author, int yearPublished, double price, boolean isPaperback) {
-        this.title = title;
-        if (!isTitleValid()) {
-            this.title = "Unknown";
-        }
+       	super(title, price);
         this.author = author;
         if (!isAuthorValid()) {
             this.author = "Unknown";
         }
         this.yearPublished = yearPublished;
-        this.price = price;
-        if (!isPriceValid()) {
-            this.price = 0;
-        }
         this.isPaperback = isPaperback;
-    }
-
-    public String getTitle() {
-        return title;
     }
 
     public String getAuthor() {
@@ -32,10 +19,6 @@ public class Book {
 
     public int getYearPublished() {
         return yearPublished;
-    }
-
-    public double getPrice() {
-        return price;
     }
 
     public boolean isPaperback() {
@@ -50,15 +33,8 @@ public class Book {
     }
 
     public String toString(){
-        return "Title: " + title + " | Author: " + author + " | Year Published: " + yearPublished + " | Price: $" + price;
-    }
-
-    public boolean isPriceValid() {
-        return price >= 0;
-    }
-
-    public boolean isTitleValid() {
-        return title != null && !title.isEmpty();
+        return super.toString() + "\nAuthor: " + author + 
+		"\nYear Published: " + yearPublished;
     }
 
     public boolean isAuthorValid() {
--- src/Item.java
+++ src/Item.java
@@ -1,7 +1,53 @@
-public interface Item {
-    public String getTitle();
-    public double getPrice();
-    public String toString();
-    public boolean isPriceValid();
-    public boolean isTitleValid();
+public abstract class Item {
+
+    private String title;
+    private double price;
+
+    public Item(String title, double price) {
+	    this.title = title;
+
+	    if (!isTitleValid()) {
+		    this.title = "Unknown";
+	    }
+
+	    this.price = price;
+
+	    if (!isPriceValid()) {
+		    this.price = 0;
+	    }
+    }
+
+    public String getTitle() {
+	    return title;
+    }
+
+    public void setTitle(String title) {
+	    this.title = title;
+	    if (!isTitleValid()) {
+		    this.title = "Unknown";
+	    }
+    }
+
+    public double getPrice() {
+	    return price;
+    }
+
+    public void setPrice(double price) {
+	    this.price = price;
+	    if (!isPriceValid()) {
+		    this.price = 0;
+	    }
+    }
+
+    public String toString() {
+	    return "Title: " + title + "\nPrice: $" + price;
+    }
+
+    public boolean isPriceValid() {
+	    return price >= 0;
+    }
+
+    public boolean isTitleValid() {
+	    return title != null && !title.isEmpty();
+    }
 }
