c53cfb15163032a4e1e9e40b5f3a218c62fd3ab5
Jonathan <jongmears@gmail.com>
2024-10-01 01:33:48 -0400

    modified User.java to use Date

--- src/User.java
+++ src/User.java
@@ -77,7 +77,8 @@ public class User {
     }
 
     public void checkout() {
-        String date = java.time.LocalDate.now().toString();
+        LocalDate d = java.time.LocalDate.now();
+	Date date = new Date(d.getYear(), d.getMonth(), d.getDayOfMonth()); 
         Order order = new Order(this, cart.getItems(), shippingAddress, billingAddress, subscription, date, "Pending");
         orders.add(order);
         cart.clear();
